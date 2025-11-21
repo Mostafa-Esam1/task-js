cd746afa634c53b07e5b540991c69fc06fcb117e
Mostafa-Esam1 <me2811075@gmail.com>
2025-11-21 11:30:27 +0200

    Update main.js - التعديل النهائي

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let phones = ["iphone11", "iphone 12", "iphone 13"];
 let prices = [400, 500, 600];
 
+//Display phones
 function ShowPhones() {
   console.clear();
   phones.forEach((phone, index) => {
@@ -8,6 +9,7 @@ function ShowPhones() {
   });
 }
 
+//Add phones
 function AddPhones() {
   let NewPhone = prompt("Enter a new phone");
   let AddPrice = +prompt("Enter price");
@@ -16,6 +18,7 @@ function AddPhones() {
   ShowPhones();
 }
 
+//Delete
 function DeletPhone() {
   let DeletePhones = +prompt("Enter index for delete");
   phones.splice(DeletePhones, 1);
@@ -23,13 +26,12 @@ function DeletPhone() {
   ShowPhones();
 }
 
+//Update
 function UpDate() {
   let Index = +prompt("Enter the index");
   let NewName = prompt("Enter UpDate for name");
   let NewPrice = +prompt("inter UpDate for Price");
   phones.splice(Index -1, 1, NewName);
   prices.splice(Index -1, 1, NewPrice);
-  //   phones.push(NewName);
-  //   prices.push(NewPrice);
   ShowPhones();
 }
